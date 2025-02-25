387bedee10410e969f785277eef09ab11b88fbeb
Jim Stroomberg <jimmy@stroomberg.me>
2025-02-25 15:52:25 +0100

    kleine aanpassing

--- guess.py
+++ guess.py
@@ -1,7 +1,7 @@
 import random
 
 def guess_the_number():
-    number = random.randint(1, 100)
+    number = random.randint(0, 100)
     attempts = 0
     print("Guess the number between 1 and 100")
 
@@ -9,7 +9,7 @@ def guess_the_number():
         try:
             guess = int(input("Enter your guess: "))
             attempts += 1
-            if guess < number:
+            if guess < number: 
                 print("Too low!")
             elif guess > number:
                 print("Too high!")
